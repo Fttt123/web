c94c53b943a0d83930e4078871069d5bd855ae68
Fttt123 <83454834+Fttt123@users.noreply.github.com>
2023-11-10 20:45:01 +0300

    Add files via upload

---
+++ BajajWeb/BajajWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BajajWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
